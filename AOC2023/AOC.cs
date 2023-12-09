aa20ec1becb0687f63583e8c660a7951e74cacea
Phillip Hunter <phunter95@gmail.com>
2023-12-09 01:35:03 -0500

    Day 9 Part 1 solved

--- AOC2023/AOC.cs
+++ AOC2023/AOC.cs
@@ -23,6 +23,7 @@ namespace AOC2023
             adventPuzzles.Add(new Day6Part2());
             adventPuzzles.Add(new Day7Part1());
             adventPuzzles.Add(new Day8Part1());
+            adventPuzzles.Add(new Day9Part1());
 
             return adventPuzzles.ToArray();
         }
---
+++ AOC2023/Puzzles/Day9Part1.cs
@@ -0,0 +1,131 @@
+﻿using AOC2023;
+using AOC2023.Models.Day9Part1;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Diagnostics;
+using System.Text;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AOC2023.Puzzles
+{
+    public class Day9Part1 : IAdventPuzzle
+    {
+        public string Name { get; }
+        public string? Solution { get; }
+
+        private const string FILENAME = "Day9.txt";
+        
+        public Day9Part1()
+        {
+            Name = "Day 9 Part 1: Mirage Maintenance";
+            Solution = "1930746032";
+        }
+
+        public PuzzleOutput GetOutput()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            #region Puzzle
+            string[] currPuzzleFileLines = File.ReadAllLines(Path.Combine(ConfigurationManager.AppSettings["PuzzleInputDirectory"], FILENAME));
+            
+            Report report = new Report();
+
+            #region Parse
+
+            for (int row = 0; row < currPuzzleFileLines.Length; row++)
+            {
+                string currPuzzleFileLine = currPuzzleFileLines[row];
+
+                SensorData sensorData = new SensorData();
+                List <long> sensorDataNumbers = new List<long>();
+
+                AOC.Log($"Current line: {row + 1} / {currPuzzleFileLines.Length}");
+                foreach(string currNumberInput in currPuzzleFileLine.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+                    sensorDataNumbers.Add(long.Parse(currNumberInput));
+                
+                sensorData.Data = sensorDataNumbers;
+
+                report.SensorDatas.Add(sensorData);
+            }
+
+            #endregion Parse
+
+            List<long> nextValues = new List<long>();
+            foreach(SensorData sensorData in report.SensorDatas)
+            {
+                nextValues.Add(sensorData.GetNextValue());
+            }
+
+            #endregion Puzzle
+
+            stopwatch.Stop();
+
+            PuzzleOutput puzzleOutput = new PuzzleOutput();
+            puzzleOutput.Result = nextValues.Sum().ToString();
+            puzzleOutput.CompletionTime = stopwatch.ElapsedMilliseconds;
+
+            return puzzleOutput;
+        }
+    }
+}
+
+namespace AOC2023.Models.Day9Part1
+{
+    public class Report
+    {
+        public List<SensorData> SensorDatas { get; set; } = new List<SensorData>();
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+    
+    public class SensorData
+    {
+        public List<long> Data { get; set; } = new List<long>();
+
+        private List<List<long>> nestedData = new List<List<long>>();
+
+        public long GetNextValue()
+        {
+            AOC.Log("\n");
+            List<long>? lastNestedDataFacet = null;
+
+            while(lastNestedDataFacet == null || lastNestedDataFacet.Any(n => n != 0))
+            {
+                if (lastNestedDataFacet == null)
+                    lastNestedDataFacet = Data;
+
+                List<long> nestedDataFacet = new List<long>();
+                for (int i = 1; i < lastNestedDataFacet.Count; i++)
+                {
+                    nestedDataFacet.Add(lastNestedDataFacet[i] - lastNestedDataFacet[i - 1]);
+                }
+                nestedData.Add(nestedDataFacet);
+                lastNestedDataFacet = nestedDataFacet;
+            }
+
+            nestedData = nestedData.Prepend(Data).ToList();
+
+            for(int i = (nestedData.Count - 1); i >= 0; i--)
+            {
+                List<long> currData = nestedData[i];
+
+                if(i == (nestedData.Count - 1)) // First iteration needs 0
+                {
+                    currData.Add(0);
+                }
+                else
+                {
+                    currData.Add(currData.Last() + nestedData[i + 1].Last());
+                }
+
+                AOC.Log($"{i} {JsonConvert.SerializeObject(currData)}");
+            }
+
+            return nestedData[0].Last();
+        }
+
+    }
+}
